dfbb2a5b946dacc95c691ff8417ad340d5af82d0
Tawhid-01 <mtworld008@gmail.com>
2026-03-24 17:16:07 +0600

    Ready for PythonAnywhere deployment

---
+++ tweet/views.py
@@ -0,0 +1,75 @@
+from django.shortcuts import render, redirect
+from .models import Tweet
+from .forms import TweetForm, UserRegisterForm
+from django.shortcuts import get_object_or_404
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import login
+
+# Create your views here.
+def index(request):
+    return render(request, 'index.html')
+
+def tweet_list(request):
+    tweets = Tweet.objects.all().order_by('-created_at')
+    return render(request, 'tweet_list.html', {'tweets': tweets})
+
+# Create Tweet.
+@login_required
+def tweet_create(request):
+    if request.method == 'POST':
+        form = TweetForm(request.POST, request.FILES)
+        if form.is_valid():
+            tweet = form.save(commit=False)
+            tweet.user = request.user
+            tweet.save()
+            return redirect('tweet_list')
+    else:
+        form = TweetForm()
+    return render(request, 'tweet_form.html', {'form': form})
+
+# Edit Tweet
+@login_required
+def tweet_edit(request,tweet_id):
+    tweet = get_object_or_404(Tweet, pk=tweet_id, user=request.user)
+    if request.method == 'POST':
+        form = TweetForm(request.POST, request.FILES, instance=tweet)
+        if form.is_valid():
+            form.save(commit=False)
+            tweet.user = request.user
+            tweet.save()
+            return redirect('tweet_list')
+    else:
+        form = TweetForm(instance=tweet)
+    return render(request, 'tweet_form.html', {'form': form})
+
+# Delete Tweet
+@login_required
+def tweet_delete(request,tweet_id):
+    tweet = get_object_or_404(Tweet, pk=tweet_id, user=request.user)
+    if request.method == 'POST':
+        tweet.delete()
+        return redirect('tweet_list')
+    return render(request, 'tweet_confirm_delete.html', {'tweet': tweet})
+
+def register(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            user.set_password(form.cleaned_data['password1'])
+            login(request, user)
+            return redirect('tweet_list')
+    else:
+        form = UserRegisterForm()
+    
+    return render(request, 'registration/register.html',{'form': form}) 
+
+
+@login_required
+def tweet_like(request, tweet_id):
+    tweet = get_object_or_404(Tweet, pk=tweet_id)
+    if tweet.likes.filter(pk=request.user.id).exists():
+        tweet.likes.remove(request.user) # Unlike
+    else:
+        tweet.likes.add(request.user) # Like
+    return redirect('tweet_list')
---
+++ twite/urls.py
@@ -0,0 +1,17 @@
+from django.shortcuts import redirect
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib.auth.urls import views as auth_views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', lambda request: redirect('tweet/', permanent=True)),
+    path('tweet/',include('tweet.urls')),
+    path('accounts/',include('django.contrib.auth.urls')),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
